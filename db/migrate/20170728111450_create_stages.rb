2caade46442cabc6bf207bf5d1835f0abbb7e6c4
alkyu92 <al_kyu92@yahoo.com>
2017-07-31 12:21:01 +0800

    update

--- db/migrate/20170728111450_create_stages.rb
+++ db/migrate/20170728111450_create_stages.rb
@@ -2,7 +2,9 @@ class CreateStages < ActiveRecord::Migration[5.0]
   def change
     create_table :stages do |t|
       t.string :name
+      t.boolean :status, default: false
 
+      t.references :opportunity, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -70,8 +70,9 @@ ActiveRecord::Schema.define(version: 20170731012047) do
 
   create_table "stages", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "status",     default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "statuses", force: :cascade do |t|
