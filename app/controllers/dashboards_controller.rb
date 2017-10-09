25917a1557b437382c682d99aae04e789fa61e01
alkyu92 <al_kyu92@yahoo.com>
2017-10-09 16:02:34 +0800

    update

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -30,10 +30,18 @@ class DashboardsController < ApplicationController
 
     @opwon = Opportunity.includes(:account).where(status: "Closed-Won").order('amount')
     @oploss = Opportunity.includes(:account).where(status: "Closed-Loss").order('amount')
+    @opopen = Opportunity.includes(:account).where(status: "Open").order('amount')
+    @opaprv = Opportunity.includes(:account).where(status: "Approved").order('amount')
+
     @opwon_sum = @opwon.sum('amount')
     @oploss_sum = @oploss.sum('amount')
+    @opopen_sum = @opopen.sum('amount')
+    @opaprv_sum = @opaprv.sum('amount')
+
     gon.opwon_sum = @opwon_sum
     gon.oploss_sum = @oploss_sum
+    gon.opopen_sum = @opopen_sum
+    gon.opaprv_sum = @opaprv_sum
 
     @acc_won_hash = {}
     Account.all.each do |acc|
@@ -48,6 +56,20 @@ class DashboardsController < ApplicationController
     end
     @acc_loss_hash = @acc_loss_hash.sort_by(&:last).reverse.take(6)
     gon.acc_loss_hash = @acc_loss_hash
+
+    @acc_open_hash = {}
+    Account.all.each do |acc|
+      @acc_open_hash[acc.account_name] = acc.opportunities.where(status: "Open").sum("amount")
+    end
+    @acc_open_hash = @acc_open_hash.sort_by(&:last).reverse.take(6)
+    gon.acc_open_hash = @acc_open_hash
+
+    @acc_approved_hash = {}
+    Account.all.each do |acc|
+      @acc_approved_hash[acc.account_name] = acc.opportunities.where(status: "Approved").sum("amount")
+    end
+    @acc_approved_hash = @acc_approved_hash.sort_by(&:last).reverse.take(6)
+    gon.acc_approved_hash = @acc_approved_hash
   end
 
 end
