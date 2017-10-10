e83b1c8b2d9f3e94a93f784de06b9591e1dd8f7c
alkyu92 <al_kyu92@yahoo.com>
2017-10-10 12:45:35 +0800

    update

--- app/models/document.rb
+++ app/models/document.rb
@@ -7,9 +7,16 @@ class Document < ApplicationRecord
                     dependent: :destroy
 
   validates_attachment_content_type :doc,
-  content_type: ["application/pdf","application/vnd.ms-excel",
- "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
- "application/msword",
- "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
- "text/plain"]
+  content_type: [
+    "application/pdf",
+    "application/vnd.ms-excel",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "text/plain",
+    "image/jpg",
+    "image/jpeg",
+    "image/png",
+    "image/gif"
+  ]
 end
