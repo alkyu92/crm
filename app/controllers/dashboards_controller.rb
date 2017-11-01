213652b5dc37bea9cebd450ac820222c687ef3da
alkyu92 <al_kyu92@yahoo.com>
2017-11-01 09:10:00 +0800

    update

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -27,25 +27,20 @@ class DashboardsController < ApplicationController
 
     @accounts = Account.order('created_at DESC').pluck(:id, :account_name, :created_at).take(6)
 
-    # @tasks = Task.joins(:opportunity).order('tasks.created_at DESC').pluck(
-    # "tasks.id", :description, "tasks.created_at", "opportunities.id").take(6)
-    # @urgent_tasks = Task.joins(:opportunity).order('due_date').where(
-    # 'due_date BETWEEN ? AND ?', 2.day.ago, 3.day.from_now).pluck(
-    # "tasks.id", :description, :due_date, "opportunities.id").take(6)
-    #
-    # @events = Event.joins(:opportunity).order('events.created_at DESC').pluck(
-    # "events.id", :description, "events.created_at", "opportunities.id").take(6)
-    # @urgent_events = Event.joins(:opportunity).order('event_date').where(
-    # 'event_date BETWEEN ? AND ?', 2.day.ago, 3.day.from_now).pluck(
-    # "events.id", :description, :event_date, "opportunities.id").take(6)
-    #
-    # @calls = Call.joins(:opportunity).order('calls.created_at DESC').pluck(
-    # "calls.id", :description, :call_datetime, "opportunities.id").take(6)
-    @tasks = []
-    @urgent_tasks = []
-    @events = []
-    @urgent_events = []
-    @calls = []
+    @tasks = Task.order('created_at DESC').pluck(
+    :id, :description, :created_at, :polytask_id).take(6)
+    @urgent_tasks = Task.order('due_date').where(
+    'due_date BETWEEN ? AND ?', 2.day.ago, 3.day.from_now).pluck(
+    :id, :description, :due_date, :polytask_id).take(6)
+
+    @events = Event.order('created_at DESC').pluck(
+    :id, :description, :created_at, :polyevent_id).take(6)
+    @urgent_events = Event.order('event_date').where(
+    'event_date BETWEEN ? AND ?', 2.day.ago, 3.day.from_now).pluck(
+    :id, :description, :event_date, :polyevent_id).take(6)
+
+    @calls = Call.order('calls.created_at DESC').pluck(
+    :id, :description, :call_datetime, :polycall_id).take(6)
 
     @contacts = Contact.order('created_at DESC').pluck(:name, :created_at).take(6)
 
